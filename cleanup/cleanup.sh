4380d74148dd96c8e6bac1941f87c69a16b530f9
Andrew Cooks <acooks@linux.com>
2017-10-15 17:23:10 +1000

    add cleanup step to remove /*-----*/ comments

--- cleanup/cleanup.sh
+++ cleanup/cleanup.sh
@@ -17,6 +17,13 @@ for f in *.c *.h ; do
 done
 
 
+# remove horizontal line breaks like /* ------ */
+sed -i 's|/\*\s*---*---*\s*\*/||g' *.c
+
+
+# Insert new cleanup steps above this comment.
+# Keep Lindent as the last step.
+
 # Use the kernel's scripts/Lindent utility
 if [ -z "$LINDENT" -o ! -x "$LINDENT" ] ; then
 	echo " You need to set the LINDENT environment variable to specify where the Lindent script is (Hint: in the kernel sources at scripts/Lindent)"
