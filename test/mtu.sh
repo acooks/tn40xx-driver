302a78bd9ee35739305fece9a811ba06d65ef105
Andrew Cooks <acooks@gmail.com>
2023-12-11 21:51:07 +1000

    rework tests to use ktap

--- test/mtu.sh
+++ test/mtu.sh
@@ -1,14 +1,26 @@
+#!/usr/bin/bash
+
 # Iterate over a list of MTU sizes
 
-[[ $(type -t now) == function ]] || source $(dirname ${BASH_SOURCE[0]})/include.sh
+[[ $(type -t now) == function ]] ||
+	source $(dirname ${BASH_SOURCE[0]})/include.sh
 
 # Lets try a few MTU sizes
 declare -a MTUS=(604 1179 1180 1499 1500 1520 1650 8900 9000 9100 16384)
-now "Walking MTU sizes: ${MTUS[@]}"
 
+ktap "KTAP version 1"
+ktap "1..${#MTUS[@]}"
+
+ktap "# Walking MTU sizes: ${MTUS[@]}"
+
+let N=1
 for MTU in ${MTUS[@]}; do
-        ip link set dev $IFNAME mtu $MTU || fail "Failed to set MTU $MTU"
-        (ip --oneline link show dev $IFNAME | grep "mtu $MTU") || fail "Failed to find expected MTU $MTU"
-        sleep 1;
+	ktap "# $MTU"
+	ip link set dev $IFNAME mtu $MTU || fail "Failed to set MTU $MTU"
+	(ip --oneline link show dev $IFNAME | grep "mtu $MTU") &&
+		ktap "ok $N MTU_$MTU" ||
+		ktap "not ok $N MTU_$MTU"
+	let N++
+	sleep 1
 done
-now "MTU walk done"
+ktap "# MTU walk done"
